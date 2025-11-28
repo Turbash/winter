68dc9c83b3db0eedfda7f9002c75488d25347917
Turbash <negirawatdeepi@gmail.com>
2025-11-28 23:26:35 +0530

    added spotify player in new tab, will add news also

--- newtab.js
+++ newtab.js
@@ -18,6 +18,8 @@ document.getElementById("search").addEventListener("keydown", (e) => {
 });
 
 const snowContainer = document.createElement("div");
+const spotifyInput = document.getElementById("spotify-input");
+spotifyInput.addEventListener("keypress", handleSpotifyInputUpdate);
 snowContainer.className = "winter-snow-container";
 document.body.appendChild(snowContainer);
 
@@ -28,4 +30,29 @@ for (let i = 0; i < 100; i++) {
   snowflake.style.animationDelay = Math.random() * 5 + "s";
   snowflake.style.opacity = Math.random();
   snowContainer.appendChild(snowflake);
+}
+
+function handleSpotifyInputUpdate(event) {
+  if (event.key === "Enter") {
+    const input = document.getElementById("spotify-input").value;
+    updateSpotifyPlayer(input);
+    document.getElementById("spotify-input").value = "";
+  }
+}
+
+function updateSpotifyPlayer(url) {
+  const frame = document.getElementById("spotify-frame");
+  let src = "";
+  const regex = /spotify\.com\/(track|album|playlist|episode)\/([a-zA-Z0-9]+)/;
+  const match = url.match(regex);
+  if (match) {
+    const type = match[1];
+    const id = match[2];
+    embedSrc = `https://open.spotify.com/embed/${type}/${id}?utm_source=generator&theme=0`;
+    frame.src = embedSrc;
+  } else {
+    alert(
+      "Invalid Spotify URL. Please copy a link from Spotify (Share -> Copy Link)."
+    );
+  }
 }
